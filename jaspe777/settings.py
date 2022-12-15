e709a7f1ae194ed5e443b940f5d7e80c0ac4bc24
juan <95schunk@gmail.com>
2022-12-15 08:37:07 -0300

    Cambio de seguridad

--- jaspe777/settings.py
+++ jaspe777/settings.py
@@ -146,7 +146,7 @@ EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER="95schunk@gmail.com"
-EMAIL_HOST_PASSWORD="kgxfmigmksgzeupa"  
+EMAIL_HOST_PASSWORD="" # Escribe aqui tu contraseña  
 
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
